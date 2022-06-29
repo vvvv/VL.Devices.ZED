87901c8ae7a968bac4dea725bf4118c0493026e3
vvvv-dottore <natan.sinigaglia@gmail.com>
2022-06-29 09:00:25 +0200

    elias added c# project with utils to access sensors data and prepared the package for nuget release

---
+++ src/CameraUtils.cs
@@ -0,0 +1,15 @@
+﻿using sl;
+using System;
+
+namespace VL.Devices.ZED
+{
+    public static class CameraUtils
+    {
+        public static SensorsData GetSensorsData(this Camera camera, TIME_REFERENCE referenceTime)
+        {
+            var sensorsData = default(SensorsData);
+            camera.GetSensorsData(ref sensorsData, referenceTime);
+            return sensorsData;
+        }
+    }
+}
